901c0ba5de896f08d108eca2683e467911cd6818
XiaoDev <jingyuan.xiao@haiwaifenqi.com>
2018-09-29 19:08:56 +0800

    l

---
+++ LocationDemo/LocationDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LocationDemo
+//
+//  Created by XiaoDev on 2018/8/6.
+//  Copyright © 2018 Xiaodev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LocationDemo/LocationDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LocationDemo
+//
+//  Created by XiaoDev on 2018/8/6.
+//  Copyright © 2018 Xiaodev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
